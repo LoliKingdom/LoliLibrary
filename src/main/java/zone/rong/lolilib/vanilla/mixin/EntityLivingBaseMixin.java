d1c5d39e049c81ceb6e7dde608d17b538ccda71d
Rongmario <ethanmahmood@gmail.com>
2020-10-13 15:09:54 +0100

    Implemented #294

---
+++ src/main/java/zone/rong/lolilib/vanilla/mixin/EntityLivingBaseMixin.java
@@ -0,0 +1,29 @@
+package zone.rong.lolilib.vanilla.mixin;
+
+import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
+
+@Mixin(EntityLivingBase.class)
+public abstract class EntityLivingBaseMixin extends Entity {
+
+    public EntityLivingBaseMixin(World worldIn) {
+        super(worldIn);
+    }
+
+    @ModifyConstant(method = "onEntityUpdate", constant = @Constant(intValue = 300))
+    private int modifyForNewBreathingMechanic(int original) {
+        int air = this.getAir();
+        return air == 300 ? 300 : replenishAirSupply(air);
+    }
+
+    private int replenishAirSupply(int air) {
+        int i = EnchantmentHelper.getRespirationModifier((EntityLivingBase) (Object) this);
+        return i > 0 && this.rand.nextInt(i + 1) > 0 ? air + i : air + 1;
+    }
+
+}
---
+++ src/main/java/zone/rong/lolilib/vanilla/mixin/GuiIngameForgeMixin.java
@@ -0,0 +1,57 @@
+package zone.rong.lolilib.vanilla.mixin;
+
+import net.minecraft.block.material.Material;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiIngame;
+import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.math.MathHelper;
+import net.minecraftforge.client.GuiIngameForge;
+import net.minecraftforge.client.event.RenderGameOverlayEvent;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
+
+import static net.minecraftforge.client.event.RenderGameOverlayEvent.ElementType.AIR;
+
+@Mixin(GuiIngameForge.class)
+public abstract class GuiIngameForgeMixin extends GuiIngame {
+
+    public GuiIngameForgeMixin(Minecraft mcIn) {
+        super(mcIn);
+    }
+
+    @Shadow protected abstract boolean pre(RenderGameOverlayEvent.ElementType type);
+
+    @Shadow protected abstract void post(RenderGameOverlayEvent.ElementType type);
+
+    @Shadow public static int right_height;
+
+    /**
+     * @author Rongmario
+     */
+    @Overwrite
+    protected void renderAir(int width, int height) {
+        if (pre(AIR)) {
+            return;
+        }
+        mc.mcProfiler.startSection("air");
+        EntityPlayer player = (EntityPlayer) this.mc.getRenderViewEntity();
+        GlStateManager.enableBlend();
+        int left = width / 2 + 91;
+        int top = height - right_height;
+        int air = player.getAir();
+        if (air < 300) {
+            int full = MathHelper.ceil((double) (air - 2) * 10.0D / 300.0D);
+            int partial = MathHelper.ceil((double) air * 10.0D / 300.0D) - full;
+            for (int i = 0; i < full + partial; ++i) {
+                drawTexturedModalRect(left - i * 8 - 9, top, (i < full ? 16 : 25), 18, 9, 9);
+            }
+        }
+        right_height += 10;
+        GlStateManager.disableBlend();
+        mc.mcProfiler.endSection();
+        post(AIR);
+    }
+
+}
