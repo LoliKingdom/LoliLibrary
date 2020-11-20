3b20023f70ead54308ecf4887208a977375b6c4c
Rongmario <ethanmahmood@gmail.com>
2020-11-20 16:14:14 +0000

    Finished BottlerRecipeMakerMixin but temporarily deprecated

--- src/main/java/zone/rong/lolilib/forestry/mixin/BottlerRecipeMakerMixin.java
+++ src/main/java/zone/rong/lolilib/forestry/mixin/BottlerRecipeMakerMixin.java
@@ -6,6 +6,8 @@ import it.unimi.dsi.fastutil.objects.ObjectArrayList;
 import mezz.jei.api.ingredients.IIngredientRegistry;
 import mezz.jei.api.ingredients.VanillaTypes;
 import net.minecraft.item.ItemStack;
+import net.minecraftforge.fluids.Fluid;
+import net.minecraftforge.fluids.FluidRegistry;
 import net.minecraftforge.fluids.FluidStack;
 import net.minecraftforge.fluids.FluidUtil;
 import net.minecraftforge.fluids.capability.CapabilityFluidHandler;
@@ -21,6 +23,8 @@ public class BottlerRecipeMakerMixin {
     /**
      * @author Rongmario
      * @reason Eliminate load time by at least half a minute.
+     *
+     * TODO: needed? Bottler now disabled as of 20-11-2020
      */
     @Overwrite(remap = false)
     public static List<BottlerRecipeWrapper> getBottlerRecipes(IIngredientRegistry ingredientRegistry) {
@@ -31,12 +35,12 @@ public class BottlerRecipeMakerMixin {
                 .forEach(s -> {
                     ItemStack drainedStack = s.copy();
                     IFluidHandlerItem fluidHandler = FluidUtil.getFluidHandler(drainedStack);
-                    FluidStack drain = fluidHandler.drain(16000, true);
-                    drainedStack = fluidHandler.getContainer();
-                    if (drain != null && drain.amount > 0) {
-                        // recipes.add(new BottlerRecipeWrapper(s, FluidStack))
-                    } else {
-
+                    for (Fluid fluid : FluidRegistry.getBucketFluids()) {
+                        FluidStack fluidStack = new FluidStack(fluid, fluidHandler.getTankProperties()[0].getCapacity());
+                        fluidHandler.fill(fluidStack, true);
+                        drainedStack = fluidHandler.getContainer();
+                        recipes.add(new BottlerRecipeWrapper(drainedStack, fluidStack, s, false));
+                        recipes.add(new BottlerRecipeWrapper(s, fluidStack, drainedStack, true));
                     }
                 });
         return recipes;
