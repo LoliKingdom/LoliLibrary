c87622f36d4002e388a5da805e030f8b945e978d
Rongmario <ethanmahmood@gmail.com>
2021-03-23 04:12:13 +0000

    Added heat and climate libraries
    - Removed unused config entry

--- src/main/java/zone/rong/lolilib/LoliLibConfig.java
+++ src/main/java/zone/rong/lolilib/LoliLibConfig.java
@@ -7,7 +7,4 @@ public class LoliLibConfig {
 
     public static final LoliLibConfig INSTANCE = new LoliLibConfig();
 
-    @Config.Comment("Enable this to use an internal StringPool. Recommend if not using Openj9, otherwise, turn this off and use '-Xenablestringconstantgc' as an argument")
-    public boolean enableLoliStringPool = false;
-
 }
