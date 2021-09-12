db4f5f59c6e3d74dccf513181949fbe16503812c
Rongmario <ethanmahmood@gmail.com>
2021-09-12 12:14:51 +0100

    Maple can now be tapped with Arboreal Extractors

--- src/main/java/zone/rong/lolilib/cofh/CoFHModule.java
+++ src/main/java/zone/rong/lolilib/cofh/CoFHModule.java
@@ -1,2 +1,27 @@
-package zone.rong.lolilib.cofh;public class CoFHModule {
+package zone.rong.lolilib.cofh;
+
+import cn.mcmod.sakura.block.BlockLoader;
+import cofh.thermalexpansion.util.managers.device.TapperManager;
+import net.minecraftforge.fluids.FluidStack;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.relauncher.Side;
+import zone.rong.lolilib.LoliModule;
+
+public class CoFHModule extends LoliModule {
+
+    @Override
+    public void onFMLPostInit(FMLPostInitializationEvent event, Side side) {
+        TapperManager.addLeafMapping(BlockLoader.MAPLE_LOG.getDefaultState(), BlockLoader.MAPLE_LEAVE_GREEN.getDefaultState());
+        TapperManager.addLeafMapping(BlockLoader.MAPLE_LOG.getDefaultState(), BlockLoader.MAPLE_LEAVE_ORANGE.getDefaultState());
+        TapperManager.addLeafMapping(BlockLoader.MAPLE_LOG.getDefaultState(), BlockLoader.MAPLE_LEAVE_RED.getDefaultState());
+        TapperManager.addLeafMapping(BlockLoader.MAPLE_LOG.getDefaultState(), BlockLoader.MAPLE_LEAVE_YELLOW.getDefaultState());
+
+        TapperManager.addLeafMapping(BlockLoader.MAPLE_LOG_SAP.getDefaultState(), BlockLoader.MAPLE_LEAVE_GREEN.getDefaultState());
+        TapperManager.addLeafMapping(BlockLoader.MAPLE_LOG_SAP.getDefaultState(), BlockLoader.MAPLE_LEAVE_ORANGE.getDefaultState());
+        TapperManager.addLeafMapping(BlockLoader.MAPLE_LOG_SAP.getDefaultState(), BlockLoader.MAPLE_LEAVE_RED.getDefaultState());
+        TapperManager.addLeafMapping(BlockLoader.MAPLE_LOG_SAP.getDefaultState(), BlockLoader.MAPLE_LEAVE_YELLOW.getDefaultState());
+
+        TapperManager.addBlockStateMapping(BlockLoader.MAPLE_LOG.getDefaultState(), new FluidStack(BlockLoader.MAPLE_SYRUP_FLUID, 40));
+        TapperManager.addBlockStateMapping(BlockLoader.MAPLE_LOG_SAP.getDefaultState(), new FluidStack(BlockLoader.MAPLE_SYRUP_FLUID, 80));
+    }
 }
--- src/main/java/zone/rong/lolilib/theaurorian/mixin/CeruleanBucketMixin.java
+++ src/main/java/zone/rong/lolilib/theaurorian/mixin/CeruleanBucketMixin.java
@@ -1,6 +1,6 @@
-package zone.rong.lolilib.aurorian.mixin;
+package zone.rong.lolilib.theaurorian.mixin;
 
-import com.elseytd.theaurorian.Items.TAItem_Tool_Cerulean_Bucket;
+import com.elseytd.theaurorian.Items.TAItem_Cerulean_Bucket;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
@@ -10,7 +10,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
 
-@Mixin(TAItem_Tool_Cerulean_Bucket.class)
+@Mixin(TAItem_Cerulean_Bucket.class)
 public abstract class CeruleanBucketMixin extends Item {
 
     @Shadow(remap = false) @Final private ItemStack empty;
